2bf3130f02dd91f3a5d0ec8116094f2d4fe00e06
aqmal101 <rofiqhidayat031@gmail.com>
2024-09-19 10:41:21 +0700

    Deskripsi perubahan pada user.php

--- app/Views/user.php
+++ app/Views/user.php
@@ -1,15 +1,32 @@
-<?php
-
-namespace App\Controllers;
-
-use App\Models\UserModel;
-
-class UserController extends BaseController
-{
-    public function index()
-    {
-        $model = new UserModel();
-        $data['users'] = $model->findAll();
-        return view('user_view', $data);
-    }
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar User</title>
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css') ?>">
+</head>
+<body>
+    <h1>Daftar User</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>Username</th>
+                <th>Alamat</th>
+                <th>No HP</th>
+                <th>Role</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user): ?>
+            <tr>
+                <td><?= esc($user['nama']) ?></td>
+                <td><?= esc($user['username']) ?></td>
+                <td><?= esc($user['alamat']) ?></td>
+                <td><?= esc($user['no_hp']) ?></td>
+                <td><?= esc($user['role']) ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
