563528af3b941b99e836ed281eccd520f262c9b4
evankepin <evanaapurwayanto264@gmail.com>
2024-09-19 11:21:11 +0700

    perubahan pada pelanggan 2

--- app/Controllers/Pelanggan.php
+++ app/Controllers/Pelanggan.php
@@ -9,7 +9,7 @@ class PelangganController extends BaseController
     public function index()
     {
         $model = new PelangganModel();
-        $data['pelanggan'] = $model->findAll(); // Mengambil semua data pelanggan
-        return view('pelanggan_view', $data); // Mengirim data ke view
+        $data['pelanggan'] = $model->findAll();
+        return view('pelanggan_view', $data); 
     }
 }
