f02bc8829d789b5ec0dfd99dda7760d13b3d2297
aqmal101 <rofiqhidayat031@gmail.com>
2024-10-03 09:21:34 +0700

    Update routes configuration

--- app/Views/tampil-users.php
+++ app/Views/tampil-users.php
@@ -53,6 +53,55 @@
         </table>
     </div>
 
+    <!-- Modal Tambah User -->
+    <div class="modal fade" id="modalTambahUsers" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Tambah Pengguna</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <!-- Form Tambah Users -->
+            <form action="<?= base_url('users/sendData') ?>" method="post">
+            <div class="form-group">
+                <label for="id">ID</label>
+                <input type="text" class="form-control" name="id" required>
+            </div>
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" name="nama" required>
+            </div>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="alamat">Alamat</label>
+                <input type="text" class="form-control" name="alamat" required>
+            </div>
+            <div class="form-group">
+                <label for="no_hp">No HP</label>
+                <input type="text" class="form-control" name="no_hp" required>
+            </div>
+            <div class="form-group">
+                <label for="role">Role</label>
+                <input type="text" class="form-control" name="role" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+            </form>
+        </div>
+        </div>
+    </div>
+    </div>
+
+
     <!-- Menambahkan Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
