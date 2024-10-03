f567f53c83010b02cc44d834790fdffc07be36c5
evankepin <evanaapurwayanto264@gmail.com>
2024-10-03 10:00:42 +0700

    pembaruan pelanggan

---
+++ app/Config/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/users', 'Users::index');
+$routes->get('/barang', 'Barang::index');
+$routes->get('/transaksi', 'Transaksi::index');
+$routes->get('/pelanggan', 'Pelanggan::index');
+
+$routes->post('/pelanggan/tambah', 'Pelanggan::sendData');
+$routes->post('/users/tambah', 'Users::sendData');
+$routes->get('/users/tambah', 'Users::tambahUsers');
